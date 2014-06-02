95b8bd81a4c22a5f7ad3f5e45849ad38734275f0
Yin-Tzu Lin(阿孜孜^.^)@CML503 <known@cmlab.csie.ntu.edu.tw>
2014-06-03 00:16:10 +0800

    測試Layout Control的Form 把Layout Control加到Form裡 然後把Grpah指定給他就可以了

---
+++ project/Src/NetworkVis03/TestLayoutControl/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Smrf.NodeXL.Adapters;
+
+namespace TestLayoutControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateAndDrawGraph();
+        }
+
+        protected void PopulateAndDrawGraph()
+        {
+            IGraphAdapter oGraphAdapter = new SimpleGraphAdapter();
+            layoutControl1.Graph = oGraphAdapter.LoadGraphFromFile("..\\..\\SampleGraph.txt");
+        }
+    }
+}
