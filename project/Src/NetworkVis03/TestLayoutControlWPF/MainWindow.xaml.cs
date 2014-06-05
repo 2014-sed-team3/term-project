137bdd5f4ae940b1037ba961eb097e5c70c31121
Yin-Tzu Lin(阿孜孜^.^)@CML503 <known@cmlab.csie.ntu.edu.tw>
2014-06-05 23:15:35 +0800

    新的如何把LayoutControl加到WPF的方法 注意:LayoutControl要先compile過 才能加過來

---
+++ project/Src/NetworkVis03/TestLayoutControlWPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LayoutControls;
+using Smrf.NodeXL.Adapters;
+using Smrf.NodeXL.Core;
+
+namespace TestLayoutControlWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LayoutControl layoutControl1 ;
+        public MainWindow()
+        {
+            InitializeComponent();
+            layoutControl1 = new LayoutControl();
+            WfHost.Child = layoutControl1;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PopulateAndDrawGraph();
+        }
+        protected void PopulateAndDrawGraph()
+        {
+            IGraphAdapter oGraphAdapter = new SimpleGraphAdapter();
+            layoutControl1.Graph = oGraphAdapter.LoadGraphFromFile("..\\..\\SampleGraph.txt");
+        }
+    }
+}
