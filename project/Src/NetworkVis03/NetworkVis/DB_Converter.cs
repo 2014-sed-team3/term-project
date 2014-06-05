f7f245bcc1f8209c62a0fc4a5b34c2f115084e0f
unknown <b99902025@pclab67.pclab.csie.ntu.edu.tw>
2014-06-05 16:19:17 +0800

    Add DB_Converter.cs, need to be revised

---
+++ project/Src/NetworkVis03/NetworkVis/DB_Converter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+using Smrf.NodeXL.Core;
+
+namespace NetworkVis
+{
+    class DB_Converter
+    {
+        public Graph convert_to_graph(DataTable node_table, DataTable edge_table)
+        {
+            Graph oGraph = new Graph(GraphDirectedness.Directed);
+            IVertexCollection oVertices = oGraph.Vertices;
+            IEdgeCollection oEdges = oGraph.Edges;
+
+            DataRowCollection node_rows = node_table.Rows;
+            DataRowCollection edge_rows = edge_table.Rows;
+
+            add_nodes(node_rows, oVertices);
+            add_edges(edge_rows, oVertices, oEdges);
+
+            return oGraph;
+        }
+
+        private void add_nodes(DataRowCollection node_rows, IVertexCollection oVertices)
+        {
+            string nodename;
+            double nodeid;
+            double nodecount = 0;
+
+            foreach (DataRow row in node_rows)
+            {
+                //Notice: "nodename" and "nodeid" should be edited
+                nodename = row["nodename"].ToString();
+                nodeid = (Double)row["nodeid"];
+
+                // Add a node...
+                IVertex oVertexA = oVertices.Add();
+                oVertexA.Name = nodename;
+                oVertexA.Tag = nodeid;
+
+                nodecount++;
+            }
+        }
+
+        private void add_edges(DataRowCollection edge_rows, IVertexCollection oVertices, IEdgeCollection oEdges)
+        {
+            double from;
+            double to;
+
+            foreach (DataRow row in edge_rows)
+            {
+                //Notice: "EdgeFromid" and "EdgeToid" should be edited
+                from = (Double)row["EdgeFromid"];
+                to = (Double)row["EdgeToid"];
+
+                // Add an edge
+                IVertex oFrom = null;
+                IVertex oTo = null;
+
+                foreach (IVertex oVertex in oVertices)
+                {
+                    if (oVertex.Tag.ToString() == from.ToString())
+                    {
+                        oFrom = oVertex;
+                    }
+
+                    if (oVertex.Tag.ToString() == to.ToString())
+                    {
+                        oTo = oVertex;
+                    }
+                }
+                IEdge oEdge1 = oEdges.Add(oFrom, oTo, true);
+            }
+        }
+    }
+}
