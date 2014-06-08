854bbaf00f7223c003f952e13bf03b3b063eccab
Huang YIKE <b99902032@csie.ntu.edu.tw>
2014-06-09 05:25:24 +0800

    Can Show current network in database to UI

--- project/Src/NetworkVis03/GraphStorageManagement/NetworkID.cs
+++ project/Src/NetworkVis03/GraphStorageManagement/NetworkID.cs
@@ -12,9 +12,29 @@ namespace GraphStorageManagement
 {
     public partial class NetworkID : Form
     {
-        public NetworkID()
+
+        public NetworkID(DB_Manager db)
         {
             InitializeComponent();
+            loadNetwork(listView1, db.list_network());
+
+        }
+
+        private void loadNetwork(ListView view, DataTable result)
+        {
+            ListViewItem[] item_list = new ListViewItem[result.Rows.Count];
+            for (int i=0; i< result.Rows.Count; i++)
+            {
+                DataRow row = result.Rows[i];
+                ListViewItem newitem = new ListViewItem(row[0].ToString(), 0);
+                for (int k = 1; k < result.Columns.Count; k++)
+                {
+                    newitem.SubItems.Add(row[k].ToString());
+                }
+                item_list[i] = newitem;
+            }
+            view.Items.AddRange(item_list);
+            view.EndUpdate();
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
@@ -23,6 +43,19 @@ namespace GraphStorageManagement
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                Console.WriteLine(listView1.SelectedItems[0].Text);
+                /*TODO: Make query to generate graph*/
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
 
         }
