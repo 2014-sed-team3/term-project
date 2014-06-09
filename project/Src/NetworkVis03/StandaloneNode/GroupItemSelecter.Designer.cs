e16a4e7698c4d784102f60a699dfd3ec92704629
bittuh <jue57ks123@gmail.com>
2014-06-09 22:34:56 +0800

    delete

--- project/Src/NetworkVis03/StandaloneNode/GroupItemSelecter.Designer.cs
+++ project/Src/NetworkVis03/StandaloneNode/GroupItemSelecter.Designer.cs
@@ -47,17 +47,19 @@
             "edge reciprocation",
             "vertex eigen vector centrality",
             "group metrics"});
-            this.checkedListBox1.Location = new System.Drawing.Point(24, 12);
+            this.checkedListBox1.Location = new System.Drawing.Point(18, 10);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(229, 184);
+            this.checkedListBox1.Size = new System.Drawing.Size(173, 140);
             this.checkedListBox1.TabIndex = 0;
             this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
             // 
             // btnSelectAll
             // 
-            this.btnSelectAll.Location = new System.Drawing.Point(295, 12);
+            this.btnSelectAll.Location = new System.Drawing.Point(221, 10);
+            this.btnSelectAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSelectAll.Name = "btnSelectAll";
-            this.btnSelectAll.Size = new System.Drawing.Size(120, 44);
+            this.btnSelectAll.Size = new System.Drawing.Size(90, 35);
             this.btnSelectAll.TabIndex = 1;
             this.btnSelectAll.Text = "Select All";
             this.btnSelectAll.UseVisualStyleBackColor = true;
@@ -65,9 +67,10 @@
             // 
             // btnDeselectAll
             // 
-            this.btnDeselectAll.Location = new System.Drawing.Point(295, 62);
+            this.btnDeselectAll.Location = new System.Drawing.Point(221, 50);
+            this.btnDeselectAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDeselectAll.Name = "btnDeselectAll";
-            this.btnDeselectAll.Size = new System.Drawing.Size(120, 40);
+            this.btnDeselectAll.Size = new System.Drawing.Size(90, 32);
             this.btnDeselectAll.TabIndex = 2;
             this.btnDeselectAll.Text = "Deselect All";
             this.btnDeselectAll.UseVisualStyleBackColor = true;
@@ -75,9 +78,10 @@
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(295, 108);
+            this.btnCalculate.Location = new System.Drawing.Point(221, 86);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(120, 37);
+            this.btnCalculate.Size = new System.Drawing.Size(90, 30);
             this.btnCalculate.TabIndex = 3;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
@@ -85,9 +89,10 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(295, 151);
+            this.btnClose.Location = new System.Drawing.Point(221, 121);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(120, 41);
+            this.btnClose.Size = new System.Drawing.Size(90, 33);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -95,14 +100,15 @@
             // 
             // GroupItemSelecter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 210);
+            this.ClientSize = new System.Drawing.Size(324, 168);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.btnDeselectAll);
             this.Controls.Add(this.btnSelectAll);
             this.Controls.Add(this.checkedListBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "GroupItemSelecter";
             this.Text = "GroupItemSelecter";
             this.ResumeLayout(false);
