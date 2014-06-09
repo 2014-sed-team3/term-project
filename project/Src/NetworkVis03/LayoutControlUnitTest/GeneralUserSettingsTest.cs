a9cf8bb756da4dae9ff498b99b68506a8e8acedb
Yin-Tzu Lin(阿孜孜^.^)@CML503 <known@cmlab.csie.ntu.edu.tw>
2014-06-10 01:27:53 +0800

    陽春unit test for layout control

---
+++ project/Src/NetworkVis03/LayoutControlUnitTest/GeneralUserSettingsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection.Emit;
+using LayoutControls.UserSettings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Smrf.NodeXL.Core;
+
+namespace LayoutControlUnitTest
+{
+    [TestClass]
+    public class GeneralUserSettingsTest
+    {
+        private GeneralUserSettings m_GeneralUserSettings;
+
+        [TestMethod]
+        public void CheckDefaultSettings()
+        {
+            m_GeneralUserSettings = new GeneralUserSettings();
+            Assert.AreEqual(m_GeneralUserSettings.NewWorkbookGraphDirectedness, GraphDirectedness.Undirected);
+            Assert.AreEqual(m_GeneralUserSettings.ReadVertexLabels, true);
+            String labelSettings = (GeneralUserSettings.DefaultFont +
+                                    "\tWhite\tBottomCenter\t2147483647\t2147483647\tBlack" +
+                                    "\tTrue\t200\tBlack\t86\tMiddleCenter");
+            var conv = new LabelUserSettingsTypeConverter();
+            var lab = (LabelUserSettings) conv.ConvertFrom(labelSettings);
+            Assert.AreEqual(conv.ConvertTo(m_GeneralUserSettings.LabelUserSettings, typeof(String)), labelSettings);
+        }
+    }
+}
---
+++ project/Src/NetworkVis03/LayoutControlUnitTest/SetAndShowGraphTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LayoutControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Smrf.NodeXL.Adapters;
+
+namespace LayoutControlUnitTest
+{
+    [TestClass]
+    public class SetAndShowGraphTest
+    {
+        private LayoutControl m_layoutControl;
+        public SetAndShowGraphTest()
+        {
+            m_layoutControl = new LayoutControl();
+        }
+
+        [TestMethod]
+        public void SetAndShowGraphSuccessTest()
+        {
+            IGraphAdapter oGraphAdapter = new SimpleGraphAdapter();
+            //layoutControl1.Graph = oGraphAdapter.LoadGraphFromFile("..\\..\\SampleGraph.txt");
+            m_layoutControl.SetAndShowGraph(oGraphAdapter.LoadGraphFromFile("..\\..\\..\\SampleData\\SampleGraph.txt"));
+
+            Assert.IsNotNull(m_layoutControl.Graph);
+        }
+    }
+}
