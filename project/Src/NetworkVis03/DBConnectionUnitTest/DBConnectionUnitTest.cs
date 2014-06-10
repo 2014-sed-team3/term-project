d30dc949cfdcbdedf36c7831656338269c85f836
jcfan-tw <JeCheng.Fan@gmail.com>
2014-06-10 14:43:26 +0800

    add new UnitTest

---
+++ project/Src/NetworkVis03/DBConnectionUnitTest/DBConnectionUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StandaloneNode;
+
+namespace DBConnectionUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("The UnitTest of Importer ! \n");
+
+
+            // ==DB Connection==
+
+            // Arrange
+            bool value = true;
+            List<string> dbTestInfo = new List<string>();
+            dbTestInfo.Clear();
+            dbTestInfo.Add("localhost");
+            dbTestInfo.Add("enricolu");
+            dbTestInfo.Add("111platform!");
+            dbTestInfo.Add("test");
+
+
+            // Act
+            DBConnection db = new DBConnection(dbTestInfo);
+            value = db.checkDBExist();
+
+
+            // Assert
+            Assert.AreEqual(value, true);
+
+
+        }
+    }
+}
